c2791d6ee4bbb68e72882724752da51c5feddacb
Cam-illa <kamilla20art@gmail.com>
2021-03-28 12:42:33 +0700

    Ставим точки и кластеры

--- task2/clusterization.js
+++ task2/clusterization.js
@@ -1,19 +1,16 @@
-function getPosition(e)
-{
-    let x = y = 0;
-
-}
-
+"use strict";
 
 window.addEventListener("DOMContentLoaded", function()
 {
     var board = document.querySelector("div.workspace");
     board.addEventListener("click", function(e)
     {
-        let point = document.createElement("div");
-        point.className = "point";
-        point.style.left = e.pageX + "px";
-        point.style.top = e.pageY + "px";
-        board.appendChild(point);
+        let what = document.getElementById("radio1").checked ? "point" : "cluster";
+        let p = document.createElement("div");
+        p.className = what;
+        p.style.left = e.pageX + "px";
+        p.style.top = e.pageY + "px";
+        board.appendChild(p);        
     })
 })
+

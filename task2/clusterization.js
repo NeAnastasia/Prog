7b8ba7644ebb52e09d0b9287501f10c1a6e636c7
Cam-illa <kamilla20art@gmail.com>
2021-03-30 13:25:40 +0700

    наброски алгоритма

--- task2/clusterization.js
+++ task2/clusterization.js
@@ -1,16 +1,55 @@
-"use strict";
-
-window.addEventListener("DOMContentLoaded", function()
+window.addEventListener("DOMContentLoaded", function() 
 {
-    var board = document.querySelector("div.workspace");
-    board.addEventListener("click", function(e)
-    {
-        let what = document.getElementById("radio1").checked ? "point" : "cluster";
+    let allPoints = []; // объекты. координаты, id, массив точек
+    let allClusters = []; // объекты. координаты, id
+    let board = document.querySelector("div.workspace");
+
+    function clusterCount() {
+        if (document.getElementById('vvod').value) {
+            let clusterCount = parseInt(document.getElementById('vvod').value);
+        }
+        else {
+            alert("Введите количество кластеров");
+        }
+    }
+
+    function createClusters() {
+        // создать clusterCount кластеров и рандомно раскидать по полю
+        // сгенерировать цвет каждого кластера
+    }
+
+    function pointsToCluster() {
+        // для каждой точки найти ближайшую к ней кластер-точку и добавить ее в этот кластер
+        // т.е к объекту данного кластера добавить эту точку, а точку закрасить
+    }
+
+    function newClusterPlace() {
+        // рассчитать средние координаты в каждом кластере
+        // это новые координаты для кластера
+    }
+
+    function animated() {
+        // анимировать движение кластеров к новым координатам
+    }
+
+    
+    board.addEventListener("click", function(e) {
         let p = document.createElement("div");
-        p.className = what;
+        let pointId = "point" + allPoints.length;
+        p.className = "point";
+        p.id = pointId;
         p.style.left = e.pageX + "px";
         p.style.top = e.pageY + "px";
-        board.appendChild(p);        
+        board.appendChild(p);  
+    
+        allPoints.push({
+            x: e.pageX,
+            y: e.pageY,
+            id: pointId
+        });
     })
+
+
 })
+
 

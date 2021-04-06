916f3db0c94254082af7d6071e6bda3db25a057b
Cam-illa <kamilla20art@gmail.com>
2021-04-06 15:03:40 +0700

    Базовый вариант k-means

--- task2/clust.js
+++ task2/clust.js
@@ -3,12 +3,14 @@ window.addEventListener("DOMContentLoaded", function()
     let allPoints = []; // объекты. координаты (x, y), кластер (cluster)
     let allClusters = []; // объекты. координаты (x, y), id, цвет (color), массив точек (points)
     let clusterCount;
+    let pointCount = 0;
+    // let usePointsForCoord = [];
 
     const canvas = document.querySelector("canvas");
     const context = canvas.getContext("2d");
 
-    canvas.width = 500;
-    canvas.height = 500;
+    canvas.width = 300;
+    canvas.height = 300;
 
     function randColor() {
         var r = Math.floor(Math.random() * (256)),
@@ -17,7 +19,7 @@ window.addEventListener("DOMContentLoaded", function()
         return '#' + r.toString(16) + g.toString(16) + b.toString(16);
     }
 
-    function clustersCount() {
+    function clustersCount() { // количество кластеров
         if (document.getElementById('vvod').value) {
             clusterCount = parseInt(document.getElementById('vvod').value);
         }
@@ -26,24 +28,93 @@ window.addEventListener("DOMContentLoaded", function()
         }
     }
 
-    function createClusters() {
-        clustersCount();
-        for (let count = 0; count < clusterCount; count++) {
-            let x = Math.random() * (495 - 5) + 5;
-            let y = Math.random() * (495 - 5) + 5;
-            let color = randColor();
+    function distPointClusters(point) { // расстояние от точки до кластеров
+        let distToCluster = [];
+        allClusters.forEach(cluster => {
+            let distanceX = Math.pow(cluster.x - point.x, 2);
+            let distanceY = Math.pow(cluster.y - point.y, 2);
+            let dist = Math.pow(distanceX + distanceY, 0.5);
+            distToCluster.push(dist);
+        });
 
+        return distToCluster;
+    }
+
+    function minDistPointCluster(point) { // расстояние от точки до ближ. кластера
+        let distToCluster = distPointClusters(point);
+        let minDist = distToCluster[0];
+        for (let cluster = 1; cluster < distToCluster.length; cluster++) {
+            if (distToCluster[cluster] < minDist) {
+                minDist = distToCluster[cluster];
+            }
+        }
+
+        return minDist;
+    }
+
+    function kMeasnPlusPlus() { // начальная генерация кластеров
+        clustersCount();
+        let points = allPoints.slice();
+
+        let color = randColor();
+        allClusters.push({
+            x: points[0].x,
+            y: points[0].y,
+            color: color,
+            points: []
+        })
+        points.splice(0, 1);
+
+        for (let i = 1; i < clusterCount; i++) {
+            let distance = [];
+            let sumDist = 0;
+            points.forEach(point => {
+                let dist = minDistPointCluster(point);
+                distance.push(dist * dist);
+                sumDist = sumDist + (dist * dist);
+            });
+
+            let rnd = Math.random() * sumDist;
+            
+            let dopSum = 0;
+            let j;
+            for (j = 0; dopSum < rnd; j++) {
+                dopSum = dopSum + distance[j];
+            }
+
+            let color = randColor();
             allClusters.push({
-                x: x,
-                y: y,
+                x: points[j - 1].x,
+                y: points[j - 1].y,
                 color: color,
                 points: []
             })
+            points.splice(j - 1, 1);
+            distance.splice(j - 1, 1);
         }
+
     }
+
+    // function createClusters() {
+    //     clustersCount();
+    //     for (let count = 0; count < clusterCount; count++) {
+    //         let x = Math.random() * (canvas.width - 5) + 5;
+    //         let y = Math.random() * (canvas.width - 5) + 5;
+    //         let color = randColor();
+
+    //         allClusters.push({
+    //             x: x,
+    //             y: y,
+    //             color: color,
+    //             points: []
+    //         })
+    //     }
+
+    // }
 
     function redrawPoint(point, color) {
         context.beginPath();
+        context.globalAlpha = 1;
         context.arc(point.x, point.y, 5, 0, Math.PI * 2);
         context.fillStyle = color;
         context.fill();
@@ -53,40 +124,31 @@ window.addEventListener("DOMContentLoaded", function()
 
     function pointsToCluster() {        
         allPoints.forEach(point => {
-            let distToCluster = [];
-
-            allClusters.forEach(cluster => {
-                let distanceX = Math.pow(cluster.x - point.x, 2);
-                let distanceY = Math.pow(cluster.y - point.y, 2);
-                let dist = Math.pow(distanceX + distanceY, 0.5);
-                distToCluster.push(dist);
-            });
-
-            let indexClust = 0;
-            for (let cluster = 1; cluster < distToCluster.length; cluster++) {
-                if (distToCluster[cluster] < distToCluster[indexClust]) {
-                    indexClust = cluster;
-                }
-            }
+            let distance = distPointClusters(point);
+            let minDist = minDistPointCluster(point);
+            let indexClust = distance.indexOf(minDist);
             
             redrawPoint(point, allClusters[indexClust].color);
             allClusters[indexClust].points.push(point);
+            point.color = allClusters[indexClust].color;
         });        
     }
 
     function newClusterPlace(cluster) {
-        let coordX = 0;
-        let coordY = 0;
-        cluster.points.forEach(point => {
-            coordX = coordX + point.x;
-            coordY = coordY + point.y;
-        });
-
-        coordX =  Math.round(coordX / cluster.points.length);
-        coordY =  Math.round(coordY / cluster.points.length);
-            
-        cluster.x = coordX;
-        cluster.y = coordY;
+        if (cluster.points.length > 0) {
+            let coordX = 0;
+            let coordY = 0;
+            cluster.points.forEach(point => {
+                coordX = coordX + point.x;
+                coordY = coordY + point.y;
+            });
+    
+            coordX =  Math.round(coordX / cluster.points.length);
+            coordY =  Math.round(coordY / cluster.points.length);
+                
+            cluster.x = coordX;
+            cluster.y = coordY;            
+        }
             
     }
 
@@ -108,38 +170,87 @@ window.addEventListener("DOMContentLoaded", function()
         let y = e.clientY - rect.top;
 
         context.beginPath();
+        context.globalAlpha = 1;
         context.arc(x, y, 5, 0, Math.PI * 2);
         context.fillStyle = "black";
         context.fill();
+        context.strokeStyle = "black";
         context.stroke();
 
         allPoints.push({
             x: x,
             y: y,
-            color: ""
+            color: "black"
         });
     })
 
+    // function animate() {
+    //     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //     allPoints.forEach(point => {
+    //         redrawPoint(point, point.color);
+    //     });
+
+    //     allClusters.forEach(cluster => {
+    //         drawCluster(cluster);
+    //     });
+
+    //     pointsToCluster();
+    //     flag = true;
+    //     allClusters.forEach(cluster => {
+    //         let oldX =  Math.floor(cluster.x);
+    //         let oldY = Math.floor(cluster.y);
+    //         newClusterPlace(cluster);
+    //         if (oldX != Math.floor(cluster.x) && oldY != Math.floor(cluster.y)) {
+    //             flag = false;
+    //         }
+    //     });
+
+    //     if (flag) {
+    //         clearInterval();
+    //     }
+    // }
+
     document.getElementById("start").addEventListener("click", function(e) {
-        createClusters();
+        if ((pointCount == 0) || (pointCount == allPoints.length)) {
+            kMeasnPlusPlus();
+        }
+        
         let flag = false;
         while (!flag) {
             flag = true;
             pointsToCluster();
             allClusters.forEach(cluster => {
-                let oldX = cluster.x;
-                let oldY = cluster.y;
+                let oldX =  Math.floor(cluster.x);
+                let oldY = Math.floor(cluster.y);
                 newClusterPlace(cluster);
-                if (Math.floor(oldX) != Math.floor(cluster.x) && Math.floor(oldY) != Math.floor(cluster.y)) {
+                if (oldX != Math.floor(cluster.x) && oldY != Math.floor(cluster.y)) {
                     flag = false;
                 }
             });
+        }
+
+        if (pointCount != allPoints.length) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            allPoints.forEach(point => {
+                redrawPoint(point, point.color);
+            });            
         }
 
         allClusters.forEach(cluster => {
             drawCluster(cluster);
         });
 
+        pointCount = allPoints.length;
+
+        // setInterval(animate(), 100);
+    })
+
+    document.getElementById("clear").addEventListener("click", function(e) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        allPoints = [];
+        allClusters = [];
+        pointCount = 0;
     })
 })
 
